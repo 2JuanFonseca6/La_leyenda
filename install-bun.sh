acd476e2e1571921bcb6290fe74e333a74bc007c
2JuanFonseca6 <juanesaavila.26@gmail.com>
2025-07-23 16:36:22 -0500

    chore(bun): enhance installation script with error handling and environment setup
    - Add error handling with 'set -e' to ensure the script exits on errors.
    - Include comments for clarity on Bun installation and environment variable setup.
    - Ensure project dependencies are installed using Bun.

--- install-bun.sh
+++ install-bun.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+set -e
+
+# Instala Bun
 curl -fsSL https://bun.sh/install | bash
+
+# Exporta las variables de entorno para Bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Instala las dependencias del proyecto
 bun install 
